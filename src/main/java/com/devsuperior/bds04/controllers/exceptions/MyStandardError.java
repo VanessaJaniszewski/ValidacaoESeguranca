2eeaca2dbb37014422b0cdec745ffadd91521789
VanssaJaniszewski <vjaniszewski@gmail.com>
2023-01-04 14:47:36 -0300

    exercise for security module

---
+++ src/main/java/com/devsuperior/bds04/controllers/exceptions/MyStandardError.java
@@ -0,0 +1,21 @@
+package com.devsuperior.bds04.controllers.exceptions;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MyStandardError  implements Serializable{
+		private static final long serialVersionUID = 1L;
+		
+		private Instant timestamp;
+		private Integer status;
+		private String error;
+		private String message;
+		private String path;
+		public MyStandardError() {};
+	}
+
---
+++ src/main/java/com/devsuperior/bds04/entities/EventEntity.java
@@ -0,0 +1,78 @@
+package com.devsuperior.bds04.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_event")
+public class EventEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private LocalDate date;
+	private String url;
+	
+	@ManyToOne
+	@JoinColumn(name = "city_id")
+	private City cityId;
+	
+	public EventEntity() {
+	}
+
+	public EventEntity(Long id, String name, LocalDate date, String url, City city) {
+		this.id = id;
+		this.name = name;
+		this.date = date;
+		this.url = url;
+		this.cityId = city;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public City getCity() {
+		return cityId;
+	}
+
+	public void setCity(City city) {
+		this.cityId= city;
+	}
+}
